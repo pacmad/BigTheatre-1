b6403ef8bcd4ad0e0dc74f7a733368fff7ec6d78
Evgeen1981 <66670722+Evgeen1981@users.noreply.github.com>
2020-08-05 21:43:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+
+    $('.header-slider').slick({
+        nextArrow: '<button class="slick-next slick-arrow"><img src="images/arrow-next.svg" alt=""></button>',
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="images/arrow-prev.svg" alt=""></button>'
+    });
+
+});
